c647c9d6506fbf8505f48d6c87b6b9594b9d277a
JungHun Jang <chlj1101@gmail.com>
2021-08-18 22:30:53 +0900

    add0818

--- week6/day0817/BJ_1074.java
+++ week6/day0817/BJ_1074.java
@@ -8,40 +8,42 @@ import java.util.StringTokenizer;
 public class BJ_1074 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int N,R,C;
+	static int N, R, C;
 	static int copyN;
-	static long find;
+	static int find;
+
 	public static void main(String[] args) throws IOException {
-		st = new StringTokenizer(br.readLine()," ");
+		st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());		
+		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		if(R==0&&C==0) {
-			System.out.println("1");
-		}else {
-			copyN = N;
-			find = 1;
-			halfNhalf(R, C);//(0,0)에서 시작
-		}
+		halfNhalf(0, 0,(int)Math.pow(2, N));// (0,0)에서 시작
+
 	}
-	private static void halfNhalf(int i, int j) {
-		int MaxI = (int) Math.pow(2,N)/2;//사등분 했을 때 오른쪽 아래를 가리킴
-		int MaxJ = (int) Math.pow(2,N)/2;//사등분 했을 때 오른쪽 아래를 가리킴
-		while(true) {
-			if(MaxI<i&&MaxJ<j) {//현재 위치
-				
-			}else if(MaxI>i&&MaxJ<=j) {//위로
-				find = find + (MaxI - MaxI/2)+(MaxJ - MaxJ/2);
-				MaxI = MaxI - MaxI/2;
-				MaxJ = MaxJ + MaxJ/2;
-			}else if(MaxI<=i&&MaxJ>j) {//왼쪽으로
-				MaxI = MaxI + MaxI/2;
-				MaxJ = MaxJ - MaxJ/2;
-			}else if(MaxI>i&&MaxJ>j) {//왼쪽 위
-				MaxI = MaxI/2;
-				MaxJ = MaxJ/2;
+
+	private static void halfNhalf(int i, int j, int len) {
+		if(len==1) {
+			if(i==R && j==C){
+				System.out.println(find);
+				System.exit(0);
+			}else {
+				find++;
 			}
+			return;
 		}
+		
+		//영역 내부에 내가 찾으려는 지점이 없다면 그냥 곱하기로 퉁
+		if(!(i<=R && R<i +len&&j<=C && C<j+len)) {
+			find += len*len;
+			return;
+		}
+		
+		//4분할 탐색을 재귀적으로
+		int next = len/2;
+		halfNhalf(i, j, next);//왼쪽 위
+		halfNhalf(i, j+next, next);//오른쪽 위
+		halfNhalf(i+next, j, next);//왼쪽 아래
+		halfNhalf(i+next, j+next, next);//오른쪽 아래
 		
 	}
 
