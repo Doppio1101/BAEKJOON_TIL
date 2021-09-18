73033e155965e5d065195ed2647c048339ad23b4
JungHun Jang <chlj1101@gmail.com>
2021-09-18 23:27:21 +0900

    0918

---
+++ week10/day0918/SWEA_1767.java
@@ -0,0 +1,17 @@
+package week10.day0918;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_1767 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		
+
+	}
+}
