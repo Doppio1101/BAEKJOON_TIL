6805d6c8e6a27d969f6d8b4b52ca5a4c0c60638d
JungHun Jang <chlj1101@gmail.com>
2021-09-20 23:55:08 +0900

    0920

--- week10/day0918/SWEA_1767.java
+++ week10/day0918/SWEA_1767.java
@@ -1,22 +1,91 @@
 package week10.day0918;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class SWEA_1767 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br;
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
-
+	static int N;
+	static int[][] map;
+	static int[][] deltas = {{-1,0},{1,0},{0,-1},{0,1}};
+	static List<Point> list;
+	
 	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input_SWEA1767.txt"));
+		br = new BufferedReader(new InputStreamReader(System.in));
 		int TC = Integer.parseInt(br.readLine());
 		for(int t=1; t<=TC; t++) {
 			int result=0;
+			N = Integer.parseInt(br.readLine());
+			map = new int[N+2][N+2];
+			list = new ArrayList<>();
+			for(int i=1; i<N+1; i++) {
+				st = new StringTokenizer(br.readLine()," ");
+				for(int j=1; j<N+1; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if(map[i][j]==1) {
+						list.add(new Point(i,j));
+					}
+				}
+			}
+			//~
+			for(int i=0; i<N+2; i++) {
+				map[0][i] = -1;
+			}
 			
+			for(int i=0; i<N+2; i++) {
+				map[N+1][i] = -1;
+			}
+			for(int i=1; i<N+2; i++) {
+				map[i][0]= -1;
+				map[i][N+1] = -1;
+			}
+			//여기까지 테두리에 -1 넣기.
+			dfs(0);
 			sb.append("#"+t+" "+result+"\n");
 		}
 		System.out.println(sb.toString());
 	}
+	
+	private static void dfs(int idx) {
+		//일직선 dfs 1인 구역으로부터 -1(테두리)까지.
+		int r = list.get(idx).r;
+		int c = list.get(idx).c;
+		for(int d=0; d<4; d++) {
+			int nr=r;
+			int nc=c;
+			while(true) {
+				nr += deltas[d][0];
+				nc += deltas[d][1];
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	private static boolean isIn(int nr, int nc) {
+		return 1<=nr && nr<N+1 && 1<=nc && nc<N+1;
+	}
+	
+	static class Point{
+		int r,c;
+		int len;
+		public Point(int r, int c) {
+			super();
+			this.r = r;
+			this.c = c;
+		}
+		
+	}
+	
 }
