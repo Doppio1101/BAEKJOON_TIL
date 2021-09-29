914ab55fa359c24fde409e899d0966e5327e1501
JungHun Jang <chlj1101@gmail.com>
2021-09-29 15:38:54 +0900

    0929

--- week12/day0927/BJ_11401.java
+++ week12/day0927/BJ_11401.java
@@ -10,7 +10,7 @@ public class BJ_11401 {
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
 	static long[] DP;
-	static long mod = 1000000007;
+	static int mod = 1000000007;
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine()," ");
 		int N = Integer.parseInt(st.nextToken());
@@ -18,30 +18,38 @@ public class BJ_11401 {
 		DP = new long[N+1];
 		DP[0] = 1;
 		DP[1] = 1;
-		System.out.println(pow(2,8));
-		long result = ((fact(N)%mod)*(pow(fact(N-K)*fact(K),mod-2)%mod))%mod;
-		System.out.println(result);
+		init();	
+		System.out.println(nCr(N,K));
 	}
 
-	private static long fact(int n) {
-		if(DP[n]!=0) {
-			return DP[n];
+	static void init() {
+		
+		for(int i=2; i<DP.length; i++) {
+			DP[i] = (DP[i-1]*i) %mod;
 		}
-		return DP[n] = ((fact(n-1)%mod) * (n%mod))%mod;
 	}
-	private static long pow(long num, long mod2) {
-		if(mod2 == 0) {
+	
+	static long nCr(int N, int r) {
+		if(r==0) {
 			return 1;
 		}
-		else {
-			long temp = pow(num,mod2/2);
-			if(mod2%2 == 0) {
-				//2^4 = 2^2 * 2^2
-				return temp * temp;
-			}else {
-				//2^5 = 2^2 * 2^2 * 2
-				return temp * temp*num;
-			}
+		long l1 = DP[N];
+		long l2 = pow(DP[N-r], mod-2);
+		long l3 = pow(DP[r], mod-2);
+		long result = ((l1*l2)%mod*l3)%mod;
+		return result;
+	}
+	
+	static long pow(long a, int b) {
+		//a^b
+		if(b==1) {
+			return a;
+		}
+		long half = pow(a,b/2);
+		if(b%2==0) {
+			return (half*half)%mod;
+		}else {
+			return ((half*half)%mod * a /*원래대로라면 a%MOD지만 a가  MOD보다 크지 않다*/)%mod;
 		}
 	}
 
