d45f1245cead4b946d7da551f6b0966769788ba1
JungHun Jang <chlj1101@gmail.com>
2021-10-05 04:20:11 +0900

    1520

--- week12/day0930/BJ_1520.java
+++ week12/day0930/BJ_1520.java
@@ -3,9 +3,6 @@ package week12.day0930;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BJ_1520 {
@@ -14,7 +11,7 @@ public class BJ_1520 {
 	static StringBuilder sb = new StringBuilder();
 	static int N,M;
 	static int[][] map;
-	static boolean[][] visited;
+	static int[][] visited;
 	static int[][] deltas = {{0,-1},{0,1},{-1,0},{1,0}};
 	static int cnt;
 	public static void main(String[] args) throws IOException {
@@ -22,38 +19,45 @@ public class BJ_1520 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		visited = new boolean[N][M];
+		visited = new int[N][M];//원래 boolean 2차원 배열이었으나 시간초과로 DP형태로 바꿈.
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine()," ");
 			for(int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				visited[i][j] = -1;
 			}
 		}
 		
-		dfs(0,0);
 		
-		System.out.println(cnt);
+		int result = dfs(0,0);
+		
+		System.out.println(result);
 		
 	}
 
-	private static void dfs(int r, int c) {
-		if(r == N-1 && c == M-1) {
-			cnt++;
+	private static int dfs(int r, int c) {
+		if(r == N-1 && c == M-1) {	
 			
-			return;
+			return 1;
+			
 		}
+		
+		if(visited[r][c] != -1) {
+			return visited[r][c];
+		}
+		
+		visited[r][c] = 0;
 		
 		for(int d=0; d<4; d++) {
 			int nr = r + deltas[d][0];
 			int nc = c + deltas[d][1];
 			
-			if(isIn(nr,nc) && !visited[nr][nc] && map[r][c] > map[nr][nc]) {
-				visited[nr][nc] = true;
-				dfs(nr,nc);
-				visited[nr][nc] = false;
+			if(isIn(nr,nc) &&  map[r][c] > map[nr][nc]) {
+				visited[r][c] += dfs(nr,nc);
 			}
 		}
 		
+		return visited[r][c];
 		
 	}
 	
